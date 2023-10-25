3203916de0f52512b480ad488406b66f76ef6195
Nellylie <marie.beka01@gmail.com>
2023-10-25 17:27:00 +0200

    feat(): update routes

--- src/routessection/RoutesSection.jsx
+++ src/routessection/RoutesSection.jsx
@@ -2,15 +2,18 @@ import {Routes, Route} from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import DashboardPage from "../pages/DashboardPage";
 import LoginPage from "../pages/LoginPage";
+import HeaderComponent from "../components/HeaderComponent";
+import FooterComponent from "../components/FooterComponent";
 
 function RoutesSection(){
-
     return (
-        <Routes>
-            <Route path ="/" element = {<HomePage/>}/>
-            <Route path ="/dashboard" element = {<DashboardPage/>}/>
-            <Route path ="/login" element = {<LoginPage/>}/>
-        </Routes>
+        <>
+        <HeaderComponent /><Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/login" element={<LoginPage />} />
+        </Routes><FooterComponent />
+        </>
     )
 }
 

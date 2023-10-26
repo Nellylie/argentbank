8a675c0a76fdabc41e1cd6b17a3c422eb5593f5b
Nellylie <marie.beka01@gmail.com>
2023-10-26 12:45:04 +0200

    feat(): ajout des sections account style+ component+ tableau

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,27 @@
 import { useSelector, useDispatch } from "react-redux";
 import { profilUser } from "../reduxcode/profil/profilDispatch";
 import { useEffect } from "react";
+import Account from "../components/Account";
+
+const listAccount = [
+  {
+  title:"Argent Bank Checking (x8349)",
+  solde:"$2,082.79",
+  description:"Available Balance"
+  },
+  {
+  title:"Argent Bank Savings (x6712)",
+  solde:"$10,928.42",
+  description:"Available Balance"
+  },
+  {
+  title:"Argent Bank Credit Card (x8349)",
+  solde:"$184.30",
+  description:"Current Balance"
+  }
+]
+
+
 
 function DashboardPage(){
     const profile = useSelector((state) => state.profil.profilInfos);
@@ -20,6 +41,9 @@ function DashboardPage(){
         <h1>Welcome back<br />{profile?.firstName + " " + profile?.lastName}!</h1>
         <button className="edit-button">Edit Name</button>
       </div>
+      <h2 className="sr-only">Accounts</h2>
+      {listAccount.map((element) =>(<Account title= {element.title} solde = {element.solde} description = {element.description}/>)
+      )}
     </main>
           </div>
           )

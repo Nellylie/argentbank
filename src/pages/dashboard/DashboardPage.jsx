e8bf3c2c0457206a7db2499e7778e62885fb204f
Nellylie <marie.beka01@gmail.com>
2023-12-22 16:23:34 +0100

    feat(style): adaptation style au wireframe

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -82,7 +82,7 @@ function DashboardPage() {
 
     return (
         <div>
-            <main className="main bg-dark">
+            <main className="main bg-grey">
                 <div className="header"><h1>Welcome back</h1>
 
                     {editMode ? (
